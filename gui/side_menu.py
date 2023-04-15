144d90b81ebe1be10464d9e580a03f23de699da0
TomBenRu <mail@thomas-ruff.de>
2023-04-15 20:20:50 +0200

    Versuch einer Datenbank zur Einsatzplanung.

--- gui/side_menu.py
+++ gui/side_menu.py
@@ -1,9 +1,9 @@
-from PySide6.QtGui import QMouseEvent, QResizeEvent, Qt
-from PySide6.QtWidgets import QWidget, QApplication, QVBoxLayout, QPushButton, QGraphicsView, QGraphicsScene, \
-    QGraphicsProxyWidget, QHBoxLayout, QLabel, QSizePolicy, QSpacerItem, QScrollArea
-from PySide6.QtCore import QPropertyAnimation, QPoint, QEasingCurve, QEvent
+from typing import Literal
 
-app = QApplication()
+from PySide6.QtGui import QMouseEvent, Qt
+from PySide6.QtWidgets import QWidget, QApplication, QVBoxLayout, QPushButton, QGraphicsView, QGraphicsScene, \
+    QGraphicsProxyWidget, QHBoxLayout, QLabel, QScrollArea
+from PySide6.QtCore import QPropertyAnimation, QPoint, QEasingCurve, QEvent
 
 
 class RotatableContainer(QGraphicsView):
@@ -24,91 +24,95 @@ class RotatableContainer(QGraphicsView):
 
 
 class WidgetSideMenu(QWidget):
-    def __init__(self, parent: QWidget):
+    def __init__(self, parent: QWidget, menu_width: int, snap_width: int, align: Literal['left', 'right']):
         super().__init__(parent)
-        #self.setStyleSheet("background-color:red;border-radius:0px;")
-        self.setGeometry(-110, 0, 120, 600)
+
         self.setContentsMargins(0, 0, 0, 0)
 
         self.parent = parent
+        self.align = align
+        self.menu_width = menu_width
+        self.snap_width = snap_width
+        self.pos_x_hide = 0
+        self.pos_x_show = 0
+
+        self.set_positions()
+        self.setGeometry(self.pos_x_hide, 0, menu_width, 600)
+        self.color_buttons = '#dfdfdf'
 
         self.layout = QHBoxLayout(self)
         self.layout.setContentsMargins(0, 0, 0, 0)
         self.layout.setSpacing(0)
         self.scrollarea_fields = QScrollArea()
-        self.scrollarea_fields.setStyleSheet("background-color:red;")
+        self.scrollarea_fields.setStyleSheet("background-color:#82cdcb;")
         self.layout.addWidget(self.scrollarea_fields)
         self.container_fields = QLabel()
-        self.container_fields.setMinimumWidth(95)
+        self.container_fields.setMinimumWidth(menu_width - 20)
+        self.container_fields.setContentsMargins(20, 20, 0, 20)
         self.scrollarea_fields.setWidget(self.container_fields)
 
         self.layout_fields = QVBoxLayout(self.container_fields)
 
         self.layout_fields.setAlignment(Qt.AlignTop)
-        for i in range(1, 11):
-            button = QPushButton(f'Option {i:02}')
-            button.setStyleSheet("background-color:blue;")
-            self.layout_fields.addWidget(button)
 
         self.container_fields.setMinimumHeight(len(self.container_fields.children()) * 30)
 
-        #self.layout_fields.addStretch()
-
-        # self.lb_show_side_menu = QLabel('schow')
-        # self.lb_show_side_menu.setMinimumWidth(len(self.container_fields.children()) * 30)
-        # self.rot_container = RotatableContainer(self.lb_show_side_menu, -90)
-        # self.layout.addWidget(self.rot_container)
-
-        # self.bt_show_sidemenu = QPushButton('Sidemenu')
-        # self.bt_show_sidemenu.setMinimumWidth(400)
-        # self.bt_show_sidemenu.clicked.connect(self.animate_sidemenu)
-        # self.rot_container = RotatableContainer(self.bt_show_sidemenu, -90)
-        # self.layout.addWidget(self.rot_container)
-
         self.anim = QPropertyAnimation(self, b"pos")
 
-        parent.resizeEvent = lambda e: self.resize(self.width(), parent.height())
+        parent.resizeEvent = self.parent_resize_event
+
+    def set_positions(self):
+        if self.align == 'left':
+            self.pos_x_hide = self.snap_width - self.menu_width
+            self.pos_x_show = 0
+        elif self.align == 'right':
+            self.pos_x_hide = self.parent.width() - self.snap_width
+            self.pos_x_show = self.parent.width() - self.menu_width
+
+    def parent_resize_event(self, e):
+        self.set_positions()
+        self.setGeometry(self.pos_x_hide, 0, self.menu_width, self.parent.height())
 
     def enterEvent(self, event: QMouseEvent) -> None:
-        if self.pos().x() < 0:
-            self.animate_sidemenu()
+        self.show_sidemenu()
 
     def leaveEvent(self, event: QEvent) -> None:
-        self.animate_sidemenu()
-
-    def animate_sidemenu(self):
-        if self.pos().x() < 0:
-            self.show_sidemenu()
-        else:
-            self.hide_siedemenu()
+        self.hide_siedemenu()
 
     def show_sidemenu(self):
         self.anim.setEasingCurve(QEasingCurve.OutBounce)
-        self.anim.setEndValue(QPoint(0, 0))
+        self.anim.setEndValue(QPoint(self.pos_x_show, 0))
         self.anim.setDuration(750)
         self.anim.start()
 
     def hide_siedemenu(self):
         self.anim.setEasingCurve(QEasingCurve.OutCubic)
-        self.anim.setEndValue(QPoint(-110, 0))
+        self.anim.setEndValue(QPoint(self.pos_x_hide, 0))
         self.anim.setDuration(750)
         self.anim.start()
 
-
-class Window(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.resize(600, 600)
-        self.layout = QVBoxLayout()
-        self.setLayout(self.layout)
-
-        self.side_menu = WidgetSideMenu(self)
-
-    # def resizeEvent(self, event: QResizeEvent) -> None:
-    #     print(event.size().height())
-    #     self.child.resize(self.child.size().width(), event.size().height())
+    def add_button(self, button: QPushButton):
+        button.setStyleSheet(f"background-color: {self.color_buttons};")
+        self.layout_fields.addWidget(button)
+        self.container_fields.setMinimumHeight(len(self.container_fields.children()) * 30)
 
 
-window = Window()
-window.show()
-app.exec()
+if __name__ == '__main__':
+    app = QApplication()
+
+    class Window(QWidget):
+        def __init__(self):
+            super().__init__()
+            self.resize(600, 600)
+            self.layout = QVBoxLayout()
+            self.setLayout(self.layout)
+
+            self.side_menu = WidgetSideMenu(self, 150, 10, 'right')
+            for i in range(1, 11):
+                button = QPushButton(f'Option {i:02}')
+                self.side_menu.add_button(button)
+
+
+    window = Window()
+    window.show()
+    app.exec()

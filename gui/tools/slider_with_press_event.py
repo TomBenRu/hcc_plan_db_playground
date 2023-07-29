a20447579796ad91ad7f28324868c87e37f07ec6
tombe <mail@thomas-ruff.de>
2023-07-29 19:51:25 +0200

    SliderWithPressEvent now also for vertical orientation.

--- gui/tools/slider_with_press_event.py
+++ gui/tools/slider_with_press_event.py
@@ -3,16 +3,18 @@ from PySide6.QtWidgets import QSlider
 
 
 class SliderWithPressEvent(QSlider):
-    """only works for horizontal orientation"""
+    """works eventually also for vertical orientation"""
 
     def mousePressEvent(self, e):
-        if e.button() == Qt.LeftButton:
-            e.accept()
-            x = e.position().x()
-            value = (self.maximum() - self.minimum()) * x / self.width() + self.minimum()
-            value = round(value)
-            if value == self.value():
-                super().mousePressEvent(e)
-            self.setValue(value)
-        else:
+        if e.button() != Qt.LeftButton:
             return super().mousePressEvent(e)
+        e.accept()
+        if self.orientation() == Qt.Orientation.Horizontal:
+            position = e.position().x()
+        else:
+            position = e.position().y()
+        value = (self.maximum() - self.minimum()) * position / self.width() + self.minimum()
+        value = round(value)
+        if value == self.value():
+            super().mousePressEvent(e)
+        self.setValue(value)

5bab016fc6c7287262e9d3b696fcd83ad6afb9e1
tombe <mail@thomas-ruff.de>
2023-10-28 17:21:13 +0200

    Optimierungs-Modul weiter in Arbeit.

--- sat_solver/cast_group_tree.py
+++ sat_solver/cast_group_tree.py
@@ -6,22 +6,21 @@ from anytree import NodeMixin, RenderTree, ContRoundStyle
 from database import schemas, db_services
 
 
-class EventGroup(NodeMixin):
-    def __init__(self, event_group_db: schemas.EventGroupShow | None,
-                 children: list['EventGroup'] = None,
-                 parent: Optional['EventGroup'] = None):
+class CastGroup(NodeMixin):
+    def __init__(self, cast_group_db: schemas.CastGroupShow | None,
+                 children: list['CastGroup'] = None,
+                 parent: Optional['CastGroup'] = None):
         super().__init__()
-        self.event_group_id = event_group_db.id if event_group_db else 0
-        self.name = str(self.event_group_id)
-        self.event_group_db = event_group_db
+        self.cast_group_id = cast_group_db.id if cast_group_db else 0
+        self.name = str(self.cast_group_id)
+        self.cast_group_db = cast_group_db
         self._event = None
-        self.parent: Optional['EventGroup'] = parent
-        self.children: list['EventGroup'] = children if children is not None else []
-        self.weight = event_group_db.variation_weight if event_group_db else None
-        self.nr_of_active_children = event_group_db.nr_event_groups if event_group_db else None
-
-    def _post_detach(self, parent):
-        self.weight = None
+        self.parent: Optional['CastGroup'] = parent
+        self.children: list['CastGroup'] = children if children is not None else []
+        self.fixed_cast: str | None = cast_group_db.fixed_cast if cast_group_db else None
+        self.nr_actors: int | None = cast_group_db.nr_actors if cast_group_db else None
+        self.custom_rule: str | None = cast_group_db.custom_rule if cast_group_db else None
+        self.strict_rule_pref: int | None = cast_group_db.strict_cast_pref if cast_group_db else None
 
     @property
     def event(self) -> schemas.EventShow:
@@ -30,59 +29,52 @@ class EventGroup(NodeMixin):
         return self._event
 
     def get_event_from_db(self):
-        return (db_services.Event.get(self.event_group_db.event.id)
-                if (self.event_group_db and self.event_group_db.event) else None)
+        return (db_services.Event.get(self.cast_group_db.event.id)
+                if (self.cast_group_db and self.cast_group_db.event) else None)
 
     def __repr__(self):
-        event_date = self.event.date.strftime('%d.%m.%y') if self.event else None
-        return (f'Node id: {self.event_group_id}, weight: {self.weight}, '
-                f'nr_active_children: {self.nr_of_active_children}, event: {event_date}')
+        return f'cast group with event {self.event.date: %d.%m.%y}' if self.event else 'cast group'
 
 
-class EventGroupTree:
-    def __init__(self, location_plan_period_ids: list[UUID]):
-        self.location_plan_period_ids = location_plan_period_ids
-        self.nodes: dict[UUID | int, EventGroup] = {}
-        self.root: EventGroup = self.construct_root_node()
+class CastGroupTree:
+    def __init__(self, plan_period_id: UUID):
+        self.plan_period_id = plan_period_id
+        self.nodes: dict[UUID | int, CastGroup] = {}
+        self.root: CastGroup | None = None
 
-        self.construct_event_group_tree()
+        self.construct_cast_group_tree()
 
-    def construct_root_node(self) -> EventGroup:
-        if len(self.location_plan_period_ids) == 1:
-            event_group_db = db_services.EventGroup.get_master_from__location_plan_period(self.location_plan_period_ids[0])
-            child_groups = [db_services.EventGroup.get(evg.id) for evg in event_group_db.event_groups]
-            root = EventGroup(event_group_db, [EventGroup(child) for child in child_groups], None)
-        else:
-            child_groups = [db_services.EventGroup.get_master_from__location_plan_period(lpp_id)
-                            for lpp_id in self.location_plan_period_ids]
-            root = EventGroup(None, [EventGroup(child) for child in child_groups])
-        self.nodes[0] = root
-        self.nodes.update({n.event_group_id: n for n in root.children})
+    def construct_cast_group_tree(self):
+        self.root = CastGroup(None)
+        all_cast_groups_db = db_services.CastGroup.get_all_from__plan_period(self.plan_period_id)
+        cast_groups_db_top = [cg for cg in all_cast_groups_db if not cg.parent_groups]
+        top_nodes: list[CastGroup] = []
+        lower_nodes: list[CastGroup] = []
 
-        return root
+        for cg_top in cast_groups_db_top:
+            curr_node = CastGroup(cg_top, None, self.root)
+            if cg_top.child_groups:
+                top_nodes.append(curr_node)
 
-    def construct_event_group_tree(self):
-
-        def construct_recursive(parent: EventGroup):
-            for event_group_db in parent.event_group_db.event_groups:
-                evg = db_services.EventGroup.get(event_group_db.id)
-                child = EventGroup(evg, None, parent)
-                self.nodes[child.event_group_id] = child
-                construct_recursive(child)
-
-        for child in self.root.children:
-            construct_recursive(child)
+        while top_nodes:
+            for node in top_nodes:
+                for cg_db in node.cast_group_db.child_groups:
+                    cg_db = db_services.CastGroup.get(cg_db.id)
+                    curr_node = CastGroup(cg_db, None, node)
+                    if cg_db.child_groups:
+                        lower_nodes.append(curr_node)
+            top_nodes, lower_nodes = lower_nodes, []
 
 
-def get_event_group_tree(plan_period_id: UUID) -> EventGroupTree:
-    location_plan_periods = db_services.PlanPeriod.get(plan_period_id).location_plan_periods
-    return EventGroupTree([lpp.id for lpp in location_plan_periods])
+def get_cast_group_tree(plan_period_id: UUID) -> CastGroupTree:
+    return CastGroupTree(plan_period_id)
 
 
-def render_event_group_tree(event_group_tree: EventGroupTree):
-    print(RenderTree(event_group_tree.root, ContRoundStyle))
+def render_cast_group_tree(cast_group_tree: CastGroupTree):
+    print(RenderTree(cast_group_tree.root, ContRoundStyle))
 
 
 if __name__ == '__main__':
     PLAN_PERIOD_ID = UUID('0BD5C3876C4E48D1B84D6F395CD74C65')
-    get_event_group_tree(PLAN_PERIOD_ID)
+    cast_group_tree = get_cast_group_tree(PLAN_PERIOD_ID)
+    render_cast_group_tree(cast_group_tree)

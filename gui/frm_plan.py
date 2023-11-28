e8a2ad1605898fd9193feba42764324f29e41d65
tombe <mail@thomas-ruff.de>
2023-11-28 03:42:48 +0100

    Optimierungs-Modul weiter in Arbeit.

--- gui/frm_plan.py
+++ gui/frm_plan.py
@@ -3,7 +3,9 @@ from collections import defaultdict
 from uuid import UUID
 
 from PySide6 import QtCore
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel, QTableWidget, QTableWidgetItem, QHeaderView
+from PySide6.QtGui import Qt
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel, QTableWidget, QTableWidgetItem, QHeaderView, QGridLayout, \
+    QHBoxLayout
 
 from commands import command_base_classes
 from commands.database_commands import plan_commands
@@ -18,9 +20,24 @@ class FrmTabPlan(QWidget):
 
         self.controller = command_base_classes.ContrExecUndoRedo()
 
+        self.weekdays_names = {
+            1: 'Montag',
+            2: 'Dienstag',
+            3: 'Mittwoch',
+            4: 'Donnerstag',
+            5: 'Freitag',
+            6: 'Samstag',
+            7: 'Sonntag',
+        }
+        self.all_days_of_month = self.generate_all_days()
+        self.all_week_nums_of_month = self.generate_all_week_nums_of_month()
+        self.week_num_row = self.generate_week_num_row()
+        self.weekday_col = self.generate_weekday_col()
+
         self.week_num_weekday = self.generate_week_num_weekday()
         self.weekdays_locations = self.get_weekdays_locations()
         self.column_assignments = self.generate_column_assignments()
+        self.day_location_id_appointments = self.generate_day_appointments()
 
         self.layout = QVBoxLayout(self)
 
@@ -40,7 +57,7 @@ class FrmTabPlan(QWidget):
         return weekdays_locations
 
     def generate_weekdays_locations(self):
-        weekdays_locations: defaultdict[int, list[schemas.LocationOfWork]] = defaultdict(list)
+        weekdays_locations = {weekday_num: [] for weekday_num in self.weekdays_names}
         for appointment in self.plan.appointments:
             weekday = appointment.event.date.isoweekday()
             if (appointment.event.location_plan_period.location_of_work.id
@@ -72,72 +89,92 @@ class FrmTabPlan(QWidget):
         return column_assignments
 
     def table_plan_config(self):
-        num_rows = len(self.week_num_weekday) + 3
-        num_cols = len([loc for locations in self.weekdays_locations.values() for loc in locations]) + 2
+        num_rows = max(self.week_num_row.values()) + 1
+        num_cols = max(self.weekday_col.values()) + 1
         self.table_plan.setRowCount(num_rows)
         self.table_plan.setColumnCount(num_cols)
-        self.table_plan.horizontalHeader().setVisible(False)
-        self.display_headers_weekdays()
+        self.table_plan.setHorizontalHeaderLabels(list(self.weekdays_names.values()))
+        self.table_plan.setVerticalHeaderLabels([''] + [f'KW {wd}' for wd in self.week_num_row])
         self.display_headers_locations()
-        self.table_plan.setVerticalHeaderLabels(['', ''] + [f'KW {kw}' for kw in sorted(self.week_num_weekday.keys())])
+        self.display_days()
         self.display_appointments()
         # self.table_plan.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.ResizeToContents)
         # self.table_plan.verticalHeader().setSectionResizeMode(QHeaderView.ResizeMode.ResizeToContents)
         self.table_plan.resizeColumnsToContents()
         self.table_plan.resizeRowsToContents()
 
+    def generate_all_days(self):
+        start, end = self.plan.plan_period.start, self.plan.plan_period.end
+        days_between = (end - start).days + 1
+        return [start + datetime.timedelta(days=i) for i in range(days_between)]
 
-    def display_headers_weekdays(self):
-        weekdays_names = {
-            1: 'Montag',
-            2: 'Dienstag',
-            3: 'Mittwoch',
-            4: 'Donnerstag',
-            5: 'Freitag',
-            6: 'Samstag',
-            7: 'Sonntag',
-        }
+    def generate_all_week_nums_of_month(self):
+        return sorted({d.isocalendar()[1] for d in self.all_days_of_month})
 
-        curr_col = 0
-        for weekday in sorted(self.weekdays_locations.keys()):
-            header_item = QTableWidgetItem(weekdays_names[weekday])
-            header_item.setTextAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
-            self.table_plan.setItem(0, curr_col, header_item)
-            if (len_wd := len(self.weekdays_locations[weekday])) > 1:
-                self.table_plan.setSpan(0, curr_col, 1, len_wd)
-            curr_col += max(len(self.weekdays_locations[weekday]), 1)
-    
+    def generate_week_num_row(self):
+        return {week_num: week_num - min(self.all_week_nums_of_month) + 1 for week_num in self.all_week_nums_of_month}
+
+    def generate_weekday_col(self):
+        return {weekday: weekday - min(self.weekdays_names) for weekday in self.weekdays_names}
+
+    def generate_day_appointments(self):
+        day_location_id_appointments: defaultdict[datetime.date, defaultdict[UUID, list]] = defaultdict(lambda: defaultdict(list))
+        for appointment in self.plan.appointments:
+            date = appointment.event.date
+            location_id = appointment.event.location_plan_period.location_of_work.id
+            day_location_id_appointments[date][location_id].append(appointment)
+        return day_location_id_appointments
+
+    def display_days(self):
+        for day in self.all_days_of_month:
+            row = self.week_num_row[day.isocalendar()[1]]
+            col = self.weekday_col[day.isoweekday()]
+            widget_day = QWidget()
+            layout_day = QVBoxLayout(widget_day)
+            layout_day.setAlignment(Qt.AlignmentFlag.AlignTop)
+            layout_day.addWidget(QLabel(day.strftime('%d.%m.%y')))
+            container_locations = QWidget()
+            container_locations.setObjectName('container_locations')
+            layout_locations = QGridLayout(container_locations)
+            layout_day.addWidget(container_locations)
+            for i, _ in enumerate(self.weekdays_locations[day.isoweekday()]):
+                container_appointments = QWidget()
+                container_appointments.setFixedWidth(150)
+                layout_appointments = QVBoxLayout(container_appointments)
+                layout_locations.addWidget(container_appointments, 0, i)
+            self.table_plan.setCellWidget(row, col, widget_day)
+
     def display_headers_locations(self):
-        curr_col = 0
-        for weekday in sorted(self.weekdays_locations.keys()):
-            for i, location in enumerate(self.weekdays_locations[weekday]):
-
-                label = QLabel(f'{location.name}\n{location.address.city}')
-                label.setFixedWidth(150)
-                label.setWordWrap(True)
-                self.table_plan.setCellWidget(1, curr_col + i, label)
-            curr_col += max(len(self.weekdays_locations[weekday]), 1)
+        for weekday, locations in self.weekdays_locations.items():
+            container = QWidget()
+            container_layout = QHBoxLayout(container)
+            for location in locations:
+                widget = QLabel(f'{location.name} {location.address.city}')
+                widget.setFixedWidth(150)
+                widget.setWordWrap(True)
+                container_layout.addWidget(widget)
+            self.table_plan.setCellWidget(0, self.weekday_col[weekday], container)
 
     def display_appointments(self):
-        min_week_num = min(self.week_num_weekday.keys())
-        date_appointments: defaultdict[datetime.date, defaultdict[UUID, list[schemas.Appointment]]] = defaultdict(lambda: defaultdict(list))
-        for appointment in self.plan.appointments:
-            date_appointments[appointment.event.date][appointment.event.location_plan_period.location_of_work.id].append(appointment)
+        for day, location_ids_appointments in self.day_location_id_appointments.items():
+            row = self.week_num_row[day.isocalendar()[1]]
+            col = self.weekday_col[day.isoweekday()]
+            container_day = self.table_plan.cellWidget(row, col)
+            container_locations = container_day.findChild(QWidget, 'container_locations')
+            for loc_id, appointments in location_ids_appointments.items():
+                pos = [loc.id for loc in self.weekdays_locations[day.isoweekday()]].index(loc_id)
+                container_appointments = container_locations.layout().itemAtPosition(0, pos).widget()
+                for appointment in appointments:
+                    container_appointment = QWidget()
+                    layout_appointment = QVBoxLayout(container_appointment)
+                    container_appointments.layout().addWidget(container_appointment)
+                    lb_time_of_day = QLabel(f'{appointment.event.time_of_day.name}: '
+                                            f'{appointment.event.time_of_day.start:%H:%M}'
+                                            f'-{appointment.event.time_of_day.end:%H:%M}')
+                    employees = '\n'.join([avd.actor_plan_period.person.f_name for avd in appointment.avail_days])
+                    lb_employees = QLabel(f'{employees}')
+                    layout_appointment.addWidget(lb_time_of_day)
+                    layout_appointment.addWidget(lb_employees)
 
-        for date, dict_uuid_appointments in date_appointments.items():
-            kw, wd = date.isocalendar()[1:3]
-            for loc_id, appointments in dict_uuid_appointments.items():
-                row = kw - min_week_num + 2
-                col = self.column_assignments[wd][loc_id]
-                container = QWidget()
-                layout = QVBoxLayout(container)
-                layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
-                layout.addWidget(QLabel(date.strftime('%d.%m.%y')))
-                for appointment in sorted(appointments, key=lambda x: x.event.time_of_day.time_of_day_enum.time_index):
-                    appointment_text = (f'{appointment.event.time_of_day.name}\n'
-                                        + '\n'.join(a.actor_plan_period.person.f_name
-                                                    for a in sorted(appointment.avail_days,
-                                                                    key=lambda x: x.actor_plan_period.person.f_name)))
-                    label = QLabel(appointment_text)
-                    layout.addWidget(label)
-                self.table_plan.setCellWidget(row, col, container)
+
+

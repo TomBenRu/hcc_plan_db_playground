a82810ae80dfc6104ee2c410337395774521a24b
tombe <mail@thomas-ruff.de>
2024-07-27 20:24:53 +0200

    Optimierungs-Modul weiter in Arbeit.

--- configuration/solver.py
+++ configuration/solver.py
@@ -25,7 +25,6 @@ class ConstraintsMultipliers(BaseModel):
     sliders_levels_weights_event_groups: dict[float, int] = {1: 100, 2: 10, 3: 1}
     sliders_weights_avail_day_groups: dict[float, int] = {1: 1000, 2: 0, 3: -1}
     sliders_levels_weights_av_day_groups: dict[float, int] = {1: 100, 2: 10, 3: 1}
-    sliders_num_violations_strict_cast_pref: dict[float, int] = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5}
     partner_loc_prefs_levels: dict[float, int] = {}
     # todo: bei mehr als 2 Mitarbeitern werden die Weight-Vars angepasst. Derzeit funktional implementiert
 

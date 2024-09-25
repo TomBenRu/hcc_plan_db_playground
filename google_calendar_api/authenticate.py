3084d25c8cd7d1aed8a36adaae968f5dccc2012a
tombe <mail@thomas-ruff.de>
2024-09-26 01:31:04 +0200

    Implementierung Google Calendar

--- google_calendar_api/authenticate.py
+++ google_calendar_api/authenticate.py
@@ -19,7 +19,7 @@ def authenticate_google():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(os.path.join(credentials_dir, 'credentials',
+            flow = InstalledAppFlow.from_client_secrets_file(os.path.join(credentials_dir,
                                                                           'client_secret.json'),
                                                              SCOPES)
             creds = flow.run_local_server(port=0)

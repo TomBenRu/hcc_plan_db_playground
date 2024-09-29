19487dd0422161604ed7e4a4387441e55e016c8b
tombe <mail@thomas-ruff.de>
2024-09-30 00:36:59 +0200

    Implementierung Google Calendar und diverse sonstige Bugfixes

--- gui/frm_appointments_to_google_calendar.py
+++ gui/frm_appointments_to_google_calendar.py
@@ -6,6 +6,7 @@ from database import schemas
 class DlgSendAppointmentsToGoogleCal(QDialog):
     def __init__(self, parent: QWidget, plan: schemas.PlanShow):
         super().__init__(parent)
+        self.setWindowTitle('Termine zu Google-Kalendern übertragen')
         self.plan = plan
 
         self._setup_data()
@@ -21,10 +22,12 @@ class DlgSendAppointmentsToGoogleCal(QDialog):
         self.layout.addLayout(self.layout_body)
         self.layout.addLayout(self.layout_foot)
 
-        self.lb_description = QLabel(f'Möchten Sie die Termine des Zeitraums {self.text_time_span}\n'
-                                     'in die entsprechenden Google-Kalender der Mitarbeiter eintragen?\n'
-                                     'Bei diesem Vorgang werden vorhandene Termine dieses Zeitraums\n'
-                                     'aus den Kalendern entfernt.')
+        self.lb_description = QLabel(f'<h4>Team: {self.plan.plan_period.team.name}<br>'
+                                     f'Zeitraum: {self.text_time_span}</h4>'
+                                     f'<p>Möchten Sie die Termine dieses Zeitraums<br>'
+                                     'in die entsprechenden Google-Kalender der Mitarbeiter eintragen?<br>'
+                                     'Bei diesem Vorgang werden vorhandene Termine dieses Zeitraums '
+                                     'aus den Kalendern entfernt.</p>')
         self.layout_head.addWidget(self.lb_description)
         self.button_box = QDialogButtonBox(QDialogButtonBox.StandardButton.Yes | QDialogButtonBox.StandardButton.No)
         self.layout_foot.addWidget(self.button_box)

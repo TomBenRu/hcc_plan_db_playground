b7e97fba07fc2bf356e0adcc9cf2eb07a0455a9a
tombe <mail@thomas-ruff.de>
2024-10-07 23:56:42 +0200

    frm_location_plan_period.py: Wenn Tageszeiten aller Events zurückgesetzt werden, wird auch der jeweilige Tageszeiten-Standard gesetzt.

--- configuration/solver.py
+++ configuration/solver.py
@@ -16,7 +16,7 @@ class MinimizationWeights(BaseModel):
     constraints_location_prefs: float = 0.001  # bei Faktor 0.0001 hat location prefs keinen Einfluss, komisch!
     constraints_partner_loc_prefs: float = 0.1
     constraints_fixed_casts_conflicts: float = 1_000_000_000
-    constraints_cast_rule: float = 10
+    constraints_cast_rule: float = 1000
 
 
 class ConstraintsMultipliers(BaseModel):
@@ -24,7 +24,7 @@ class ConstraintsMultipliers(BaseModel):
     sliders_partner_loc_prefs: dict[float, int] = {0: 20, 0.5: 10, 1: 0, 1.5: -10, 2: -20}  # WEIGHT_VARS_PARTNER_LOC_PREFS
     sliders_levels_weights_event_groups: dict[float, int] = {1: 100, 2: 10, 3: 1}
     sliders_weights_avail_day_groups: dict[float, int] = {1: 1000, 2: 0, 3: -1}
-    sliders_levels_weights_av_day_groups: dict[float, int] = {1: 100, 2: 10, 3: 1}
+    sliders_levels_weights_av_day_groups: dict[float, int] = {1: 1, 2: 1, 3: 1}
     partner_loc_prefs_levels: dict[float, int] = {}
     # todo: bei mehr als 2 Mitarbeitern werden die Weight-Vars angepasst. Derzeit funktional implementiert
 
